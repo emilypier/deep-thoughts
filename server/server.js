20eef0c0669235c811db440dbc87ccd8661b6adf
emilypier <piersoee@miamioh.edu>
2022-03-22 12:58:40 -0400

    add mutations for thoughts, friends, & reactions

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -14,7 +14,7 @@ const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
-    // context: authMiddleware commented out bc nodemon wouldn't run w it
+    context: authMiddleware
   });
 
   // Start the Apollo server
