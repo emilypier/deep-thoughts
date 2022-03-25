758e7e85685445f984fcb5ac0d891698eaf4314d
emilypier <piersoee@miamioh.edu>
2022-03-25 18:01:27 -0400

    customize homepage for logged-in users

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -22,8 +24,19 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+// _ serves as a placeholder for first parameter
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
